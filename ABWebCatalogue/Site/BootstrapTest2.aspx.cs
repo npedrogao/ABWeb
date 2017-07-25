80b2d06b5822e0ab960c19a7ecc60ab8033a40dd
Nuno <nunopedrogao@gmail.com>
2017-07-25 11:17:53 +0100

    Boostrap vs 3_02 Jquery 1.8.3

---
+++ ABWebCatalogue/Site/BootstrapTest2.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ABWebCatalogue.Site
+{
+    public partial class BootstrapTest2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
