70a78c3ea6c2e7ee0c11bb74cd1209c9474d5beb
jrtsa <jrafaelts@gmail.com>
2017-07-27 12:10:55 +0100

    Get Error Code por filtro

---
+++ Core/Models/ErrorCodeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Models
+{
+    public class ErrorCodeModel
+    {
+        public int CodigoErro { get; set; }
+        public string Mensagem { get; set; }
+        public string Help { get; set; }        
+    }
+}
