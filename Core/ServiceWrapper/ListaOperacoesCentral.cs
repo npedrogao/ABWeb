e623db6c990acfaa1850a288fc8620f9af71bcd1
jrtsa <jrafaelts@gmail.com>
2017-07-28 18:26:27 +0100

    ListaOperacoesCentral

---
+++ Core/ServiceWrapper/ListaOperacoesCentral.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.ServiceWrapper
+{
+    public class ListaOperacoesCentral
+    {
+        //TODO
+        //        AO03 - Agenda de Ordens de Fundos
+        //C, M, V
+        //**********************************************************
+        //AP69 - Depósitos Estruturados.
+        //A, C, M, V
+        //***************************************************************
+        //CA25 - Fundos
+        //C
+        //*****************************************************************
+        //FU01 - Ordem de Subscrição de Fundos
+        //******************************************************************
+        //FU04 – Subscrição/Transferência de Entidade externa
+        //C
+        //*********************************************************************
+        //FU06 - Ordem de Subscrição de Fundos em Moeda Estrangeira
+        //A, C, M, V
+        //****************************************************************************
+        //TI04 - Títulos
+        //A, C, M, V
+        //******************************************************************************
+        //TI58 - Compra em bolsa e fora de Bolsa
+        //C, K, M, V
+        //***************************************************************************************
+        //TI97 = TI05 - Títulos
+        //A, C, M, V
+        //*************************************************************************************
+        //VC25 =
+        //A, C, M, V
+        //*******************************************************************************
+        //VC37 - Seguros
+        //A, C, M, V
+        //******************************************************************************************
+        //                RASCUNHO
+
+
+        //x AO03 - Agenda de Ordens de Fundos
+        //V - FU01 - Ordem de Subscrição de Fundos
+        //V - FU03 – Ordem Transferência de Fundos
+        //V - FU04 – Subscrição/Transferência de Entidade externa
+        //V - FU06 - Ordem de Subscrição de Fundos em Moeda Estrangeira
+        //V - FU08 – Depósito Fundos sem movimentação em contas D.O.
+        //TI58 - Compras em Bolsa
+        //V - TI84 - Ofertas Proprietárias
+        //TI87 - Aumento Capital, Subscrição
+        //TI92 - OPV/OPS
+        //x AP20 - Constituição de depósitos a prazo
+        //x AP21 - Constituição de depósitos a prazo
+        //V - AP81 - Subscrição de Produto Estruturado
+        //x VI06 – Reforços Seguros
+        //x VI17 – Reforços Seguros
+        //x VI11 - Agenda de Ordens Seguros
+        //x VI36 - Alteração de Modalidade Seguros
+        //x VI43 – Alteração de Modalidade Seguros
+        //x VI85 – Subscrição Seguros
+        //******************************************************************************************
+    }
+}
