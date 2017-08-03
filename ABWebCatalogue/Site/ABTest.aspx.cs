182f6f110f7ae448afedc23b20ed51f23b821b91
Nuno <nunopedrogao@gmail.com>
2017-08-03 16:59:03 +0100

    Teste em AB

--- ABWebCatalogue/Site/ABTest.aspx.cs
+++ ABWebCatalogue/Site/ABTest.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using Core.Models;
+using Core.Utils;
 using Core.WebExtensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,7 +15,11 @@ namespace ABWebCatalogue.Site
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            txtQMAXREN.Focus();
             cmbIRenovac.LoadWithList(IsPostBack, CatalogueModel.ListSimNao);
+            var builder = new StringBuilder();
+            JsUtil.ExecJsFunction("TestMethod", builder);
+            JsUtil.InjectJsServerSide(this, builder);
         }
     }
 }
---
+++ ABWebCatalogue/js/ABTest.js
@@ -0,0 +1,13 @@
+﻿function TestMethod() {
+    //$("#txtQMAXREN").focus();
+
+    //Event.observe(window, 'load', function () {
+    //    $("#txtQMAXREN").focus();
+    //});
+
+    $(document).keydown(function (e) {
+        
+        e.preventDefault();
+        alert(String.fromCharCode(e.keyCode));
+    });
+}
